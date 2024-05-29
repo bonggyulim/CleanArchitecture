cba5a09575a649ffc1a2440d588b1f55590d708d
bonggyulim <qhdrbdhkdwks@naver.com>
2024-05-29 17:04:06 +0900

    commit

--- app/src/main/java/com/example/hilt/domain/usecase/GetAllVideoUseCase.kt
+++ app/src/main/java/com/example/hilt/domain/usecase/GetAllVideoUseCase.kt
@@ -2,13 +2,11 @@ package com.example.hilt.domain.usecase
 
 import com.example.hilt.data.database.entity.LikeVideoEntity
 import com.example.hilt.domain.repository.CacheRepository
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class GetAllVideoUseCase @Inject constructor(private val cacheRepository: CacheRepository) {
-    // LiveData -> Flow invoke 함수가 바뀜, Flow는 lifecycle을 모르기에 그런듯?
+    // LiveData -> Flow : invoke 함수가 바뀜, Flow는 lifecycle을 모르기에 그런듯?
     fun invoke(): Flow<List<LikeVideoEntity>> {
         return cacheRepository.getAllVideo()
     }
--- app/src/main/java/com/example/hilt/presentation/mypage/main/MyPageViewModel.kt
+++ app/src/main/java/com/example/hilt/presentation/mypage/main/MyPageViewModel.kt
@@ -17,8 +17,11 @@ import javax.inject.Inject
 class MyPageViewModel @Inject constructor(
     private val getAllVideoUseCase: GetAllVideoUseCase
 ) : ViewModel() {
-    private val _getLikeVideoList = MutableSharedFlow<List<LikeVideoEntity>>(replay = 1) // ??
+    private val _getLikeVideoList = MutableSharedFlow<List<LikeVideoEntity>>(replay = 1)
     val getLikeVideoList: SharedFlow<List<LikeVideoEntity>> get() = _getLikeVideoList.asSharedFlow()
+
+    //shared는 데이터가 몇 개 까지 값을 캐싱하고 있을지에 대해 인자로 정의 가능
+    //state는 livedata랑 비슷 shared가 1일때랑 비슷
 
     init {
         fetchLikeVideoList()
